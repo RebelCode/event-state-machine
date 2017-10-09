cbd0e9d59c0ec74a7da8fc673bb900156186ae1d
Miguel Muscat <miguelmuscat93@gmail.com>
2017-10-09 17:18:48 +0200

    Added `AbstractEventStateMachine`

---
+++ src/AbstractEventStateMachine.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace RebelCode\State;
+
+use Dhii\Events\TransitionEventInterface;
+use Dhii\Util\String\StringableInterface as Stringable;
+use Exception;
+use Psr\EventManager\EventManagerInterface;
+
+abstract class AbstractEventStateMachine
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    protected function _transition($transition)
+    {
+        $event = $this->_getTransitionEvent($transition);
+
+        try {
+            $this->_getEventManager()->trigger($event);
+        } catch (Exception $ex) {
+            $exception = $this->_createCouldNotTransitionException(
+                $this->__('The triggered event threw an exception'),
+                null,
+                $ex,
+                $transition
+            );
+        }
+
+        if (!$event->isTransitionAborted()) {
+            $this->_setState($this->_getNewState($event));
+        }
+
+        if (isset($exception) && $exception instanceof Exception) {
+            throw $exception;
+        }
+    }
+
+    /**
+     * Retrieves the event manager associated with this instance.
+     *
+     * @since [*next-version*]
+     *
+     * @return EventManagerInterface The event manager instance.
+     */
+    abstract protected function _getEventManager();
+
+    /**
+     * Retrieves the transition event instance for a transition.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable $transition The transition related to the transition event.
+     *
+     * @return TransitionEventInterface The transition event instance.
+     */
+    abstract protected function _getTransitionEvent($transition);
+
+    /**
+     * Retrieves the new state for a given event transition.
+     *
+     * @since [*next-version*]
+     *
+     * @param TransitionEventInterface $event The event.
+     *
+     * @return string|Stringable The new state.
+     */
+    abstract protected function _getNewState(TransitionEventInterface $event);
+
+    /**
+     * Sets the state for this instance.
+     *
+     * @since [*next-version*]
+     *
+     * @param string|Stringable|null $state The state, or null.
+     */
+    abstract protected function _setState($state);
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    abstract protected function _createCouldNotTransitionException(
+        $message = null,
+        $code = null,
+        Exception $previous = null,
+        $transition = null
+    );
+
+    /**
+     * Translates a string, and replaces placeholders.
+     *
+     * @since [*next-version*]
+     * @see   sprintf()
+     * @see   _translate()
+     *
+     * @param string $string  The format string to translate.
+     * @param array  $args    Placeholder values to replace in the string.
+     * @param mixed  $context The context for translation.
+     *
+     * @return string The translated string.
+     */
+    abstract protected function __($string, $args = [], $context = null);
+}
